bb5b92b31d4053baa3b0da12196327c00ea22eda
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-16 15:54:20 +0100

    add controller

--- index.php
+++ index.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 //include all your model files here
 require_once 'Model/Entity.php';
 require 'Model/Teacher.php';
+<<<<<<< HEAD
 /*require 'Model/User.php';
 require 'Model/Student.php';*/
 
+=======
+require 'Model/User.php';
+require 'Model/SchoolClass.php';
+>>>>>>> 9ef3211b5229518c5ae1d180eed0ade71bb8bfbe
 
 //include all your controllers here
 require 'Controller/HomepageController.php';
 require 'Controller/InfoController.php';
+<<<<<<< HEAD
 /*require 'Controller/ClassController.php';
 require 'Controller/StudentController.php';
 require 'Controller/TeacherController.php';*/
+=======
+require 'Controller/TeacherController.php';
+require 'Controller/ClassController.php';
+>>>>>>> 9ef3211b5229518c5ae1d180eed0ade71bb8bfbe
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
@@ -21,6 +31,29 @@ require 'Controller/TeacherController.php';*/
 $controller = new HomepageController();
 if(isset($_GET['page']) && $_GET['page'] === 'info') {
     $controller = new InfoController();
+}
+
+if(isset($_GET['page']) && $_GET['page'] === 'classes') {
+    $controller = new ClassController();
+}
+
+if(isset($_GET['page']))
+{
+    SWITCH($_GET['page']){
+        case('info'):
+            $controller = new InfoController();
+            break;
+        case('classes'):
+            $controller = new ClassController();
+            break;
+        case('teachers'):
+            $controller = new TeacherController();
+            break;
+        case('students'):
+            $controller = new StudentController();
+            break;
+        default: break;
+    }
 }
 
 

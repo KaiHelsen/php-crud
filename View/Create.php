aed5fb8db85070f06d17b2c95f4e2885c657d41f
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-16 16:07:15 +0100

    add visual part

---
+++ View/Create.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+if (isset($_POST['submit'])) {
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $firstname = validate($_POST['firstname']);
+    $lastname = validate($_POST['lastname']);
+    $email = validate($_POST['email']);
+
+    $user_data = 'firstname = '.$firstname.' lastname = '.$lastname.' & email address = '.$email;
+
+    if (empty($firstname)) {
+        header("location:create.php?error= firstname is required&$user_data");
+    } else if (empty($lastname)) {
+        header("location:create.php?error= lastname is required&$user_data");
+    } else if (empty($email)) {
+        header("location:create.php?error= email is required&$user_data");
+    } else {
+        echo "Success" ;
+    }
+}
+
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <form action="" method="POST">
+        <h4 class="display-4 text-center">Create</h4><hr><br/>
+        <?php if (isset($_GET['error'])) {?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo $_GET['error'] ?>
+            </div>
+        <?php } ?>
+        <div class="form-group">
+            <label for="firstname">Firstname</label>
+            <input type="text" class="form-control" id="firstname" name="firstname"
+                   value="<?php if(isset($_GET['firstname'])) echo ($_GET['firstname']); ?>"
+                   placeholder="Enter your firstname">
+        </div>
+
+        <div class="form-group">
+            <label for="lastname">Lastname</label>
+            <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Enter your lastname">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email">
+        </div>
+
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+    </form>
+
+</div>
+
+</body>
+</html>
---
+++ View/studentOverview.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="box">
+        <h4 class="display-4 text-center">Student</h4>
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Firstname</th>
+                <th scope="col">Lastname</th>
+                <th scope="col">Class</th>
+                <th scope="col">Assign teacher</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>Lamaar</td>
+                <td>Koen</td>
+                <td><button type="button" name="edit">edit</button></td>
+                <td><button type="button" name="delete">delete</button></td>
+            </tr>
+            </tbody>
+        </table>
+        <div class="link-right">
+            <a href="view/Create.php" class="create">Create New</a>
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>

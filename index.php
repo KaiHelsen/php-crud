9aa9ff86891daf2ec16671d4cadcdf130ebf8622
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-16 15:34:18 +0100

    add create page

--- index.php
+++ index.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 //include all your model files here
 require_once 'Model/Entity.php';
 require 'Model/Teacher.php';
-require 'Model/User.php';
+/*require 'Model/User.php';
+require 'Model/Student.php';*/
+
 
 //include all your controllers here
 require 'Controller/HomepageController.php';
 require 'Controller/InfoController.php';
+/*require 'Controller/ClassController.php';
+require 'Controller/StudentController.php';
+require 'Controller/TeacherController.php';*/
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
@@ -20,3 +25,8 @@ if(isset($_GET['page']) && $_GET['page'] === 'info') {
 
 
 $controller->render($_GET, $_POST);
+
+
+
+
+
